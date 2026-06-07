0004b53f9a47de98c5aff70ba04b2b85f2056c85
Omar Tegany <omartegany8@gmail.com>
2026-06-08 01:08:01 +0300

    Update bot name and newsletter information

--- plugins/info/info.js
+++ plugins/info/info.js
@@ -26,8 +26,8 @@ const handler = async (m, { conn, bot, config }) => {
   const subConnected = subBots?.list().filter(b => b.connected).length || 0;
   
   const msg = `
-——> *الـبـوت 🎪*
-- *الاسم:* \`${conn.user.name || bot.config.info.nameBot || "User"}\`
+——> *الـبـوت 🕷️*
+- *الاسم:* \`${conn.user.name || bot.config.info.nameBot || "𓆩  𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪"}\`
 - *الرقم:* \`wa.me/+${conn.user.id.split(':')[0]}\`
 - *شغال منذ:* \`${uptimeHours.toString().padStart(2, '0')}:${uptimeMins.toString().padStart(2, '0')}:${uptimeSecs.toString().padStart(2, '0')}\`
 
@@ -46,16 +46,16 @@ const handler = async (m, { conn, bot, config }) => {
 ——> *احـصـائـيـات 📊*
 - *المجموعات:* \`${groupCount}\`
 
-——> *الـبـوتـات الـفـرعـيـه 🎪*
+——> *الـبـوتـات الـفـرعـيه 🎪*
 - *الإجمالي:* \`${subCount}\`
 - *المتصل:* \`${subConnected}\`
 - *المنفصل:* \`${subCount - subConnected}\`
 
 ——> *الـمـالـكـيـن 👑*
 - *العدد:* \`${bot.owners?.length || 0}\`
-- *الرئيسي:* \`${bot.owners?.[0]?.name || 'VA'} (${bot.owners?.[0]?.jid?.split('@')[0] || 'لا يوجد'})\`
+- *الرئيسي:* \`${bot.owners?.[0]?.name || 'Omar Tegany'} (${bot.owners?.[0]?.jid?.split('@')[0] || 'لا يوجد'})\`
 
-> *_VII7 BOT SYSTEM_*`;
+> *_𓆩  𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪 BOT SYSTEM_*`;
 
   await conn.sendMessage(m.chat, {
     text: msg,
@@ -72,15 +72,15 @@ const context = (jid, img) => ({
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363225356834044@newsletter',
-        newsletterName: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+        newsletterJid: '0029Vb8Q2Q56WaKx5Qk8QM2y@newsletter',
+        newsletterName: '𓆩  𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪 🕷️',
         serverMessageId: 0
     },
     externalAdReply: {
-        title: "𝐏𝐎𝐌𝐍𝐈-𝐀𝐈 🎪 | 𝐁𝐨𝐭 𝐢𝐬 𝐛𝐮𝐢𝐥𝐭 𝐨𝐧 𝐭𝐡𝐞 𝐖𝐒/𝐕𝐈𝐈 𝐟𝐫𝐚𝐦𝐞𝐰𝐨𝐫𝐤",
-        body: "𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 𝚋𝚘𝚝 𝚝𝚑𝚊𝚝 𝚒𝚜 𝚎𝚊𝚜𝚢 𝚝𝚘 𝚖𝚘𝚍𝚒𝚏𝚢 𝚊𝚗𝚍 𝚟𝚎𝚛𝚢 𝚏𝚊𝚜𝚝",
+        title: "𓆩  𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪 🕷️",
+        body: "✮⃝🕸️𝑴𝑬𝑹𝑶 𝑨𝑰✮⃝🕷️",
         thumbnailUrl: img,
-        sourceUrl: '',
+        sourceUrl: 'https://whatsapp.com/channel/0029Vb8Q2Q56WaKx5Qk8QM2y',
         mediaType: 1,
         renderLargerThumbnail: true
     }

b0fdcd198a14588b765b556630e9edeb2d811c61
Omar Tegany <omartegany8@gmail.com>
2026-06-08 03:13:58 +0300

    Update yt-button.js

--- plugins/download/yt-button.js
+++ plugins/download/yt-button.js
@@ -14,8 +14,8 @@ const handler = async (m, { conn, text }) => {
             { name: "quick_reply", params: { display_text: "🎬 ╎ تـحـمـيـل فـيـديـو", id: `.يوتيوب ${url}` } }
         ],
         mentions: [m.sender],
-        newsletter: { name: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", jid: "120363225356834044@newsletter" },
-        interactiveConfig: { buttons_limits: 10, list_title: "Pomni 🎀", button_title: "Pomni 🎀", canonical_url: url }
+        newsletter: { name: "𓆩  𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪 🕷️", jid: "120363225356834044@newsletter" },
+        interactiveConfig: { buttons_limits: 10, list_title: "MERO AI 🕷️", button_title: "MERO AI 🕷️", canonical_url: url }
     }, m);
 };
 

892967e4c07acd9d37ef592a4353dad89c1dd672
Omar Tegany <omartegany8@gmail.com>
2026-06-08 05:25:10 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, bot }) => {
-  let watermark = 'VA';
+  let watermark = '𓆩𝑴𝑬𝑹𝑶𓆪 🕷️';
   
   let quoted = {
     key: { fromMe: false, participant: '0@s.whatsapp.net', remoteJid: 'status@broadcast' },
-    message: { conversation: 'Pomni AI ❤️💙' }
+    message: { conversation: '𓆩𝑴𝑬𝑹𝑶 𝑨𝑰𓆪 🕷️' }
   };
   const num = bot.config.owners[0].jid.split("@")[0];
   let vcard = `BEGIN:VCARD
@@ -12,14 +12,14 @@ FN:${watermark}
 TEL;type=CELL;waid=${num}:+${num}
 END:VCARD`;
 
-  let img = 'https://i.pinimg.com/originals/e2/21/20/e221203f319df949ee65585a657501a2.jpg';
+  let img = 'https://accounts.google.com/SignOutOptions?hl=ar&continue=https://myaccount.google.com/%3Fhl%3Dar%26utm_source%3DOGB%26utm_medium%3Dact%26gar%3DWzJd&ec=GBRAwAE';
   
   await conn.sendMessage(m.chat, {
     contacts: { displayName: watermark, contacts: [{ vcard }] },
     contextInfo: {
       forwardingScore: 2023,
       externalAdReply: {
-        title: '𝑇𝛨𝛯 𝛩𝑊𝛮𝛯𝑅',
+        title: '𓆩  𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪 🕷️',
         body: watermark,
         sourceUrl: 'https://whatsapp.com/channel/0029Vb3UUKz3QxS3bgWmTc3x',
         thumbnailUrl: img,

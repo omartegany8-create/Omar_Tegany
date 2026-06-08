6dd5cfef5eebd74c93c003c5db8ac756382ab455
Omar Tegany <omartegany8@gmail.com>
2026-06-08 05:27:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
   fromMe: true, // 🌟 تم التعديل لـ true عشان البوت يرد على رسايلك ويسمع كلامك أنت كمان
   owners: [
     // المطور (عمر)
-    { name: "Omar", lid: "201158601817@lid", jid: "201158601817@s.whatsapp.net" },
+    { name: "𓆩  𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪 🕷️", lid: "201158601817@lid", jid: "201158601817@s.whatsapp.net" },
     { name: "Omar", lid: "201031044377@lid", jid: "201158601817@s.whatsapp.net" },
     
   ],

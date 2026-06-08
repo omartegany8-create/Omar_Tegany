a28f855e8a41e37733d90e87d7173385b870b73a
Omar Tegany <omartegany8@gmail.com>
2026-06-08 03:10:54 +0300

    Update example4.js

--- plugins/example/example4.js
+++ plugins/example/example4.js
@@ -54,7 +54,7 @@ await conn.sendCarousel(m.chat, {
   ],
   mentions: [m.sender],
   newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      name: '𓆩  𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪 🕷️',
       jid: '120363225356834044@newsletter'
     },
 }, m)

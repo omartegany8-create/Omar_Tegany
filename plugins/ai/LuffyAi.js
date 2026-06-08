e1027bf78863d5525f7a72e04db152fa482f1cd3
Omar Tegany <omartegany8@gmail.com>
2026-06-08 13:39:59 +0300

    Update LuffyAi.js

--- plugins/ai/LuffyAi.js
+++ plugins/ai/LuffyAi.js
@@ -36,7 +36,7 @@ const context = (jid, img) => ({
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363225356834044@newsletter',
+        newsletterJid: '',
         newsletterName: '𝐋𝐔𝐅𝐅𝐘 ~ 𝐊𝐢𝐧𝐠 👑',
         serverMessageId: 0
     },

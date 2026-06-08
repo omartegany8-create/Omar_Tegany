1dc775947f4022562a4cf359a229e19c90531847
Omar Tegany <omartegany8@gmail.com>
2026-06-08 06:14:38 +0300

    Update ping message and newsletter details

--- plugins/info/ping.js
+++ plugins/info/ping.js
@@ -7,9 +7,9 @@ const handler = async (m, { conn }) => {
   await conn.msgUrl(m.chat, `⚡ سرعة البوت: ${ping.toFixed(2)}ms`, {
     img: "https://i.pinimg.com/736x/73/56/32/735632c6fa8e665c249abbc8a340b77d.jpg",
     title: "𝐒𝐩𝐞𝐞𝐝 / 𝐓𝐞𝐬𝐭",
-    body: "𝐓𝐞𝐬𝐭𝐢𝐧𝐠 𝐭𝐡𝐞 𝐛𝐨𝐭'𝐬 𝐬𝐩𝐞𝐞𝐝: 𝐈𝐬 𝐢𝐭 𝐟𝐚𝐬𝐭 𝐨𝐫 𝐧𝐨𝐭?",
+    body: "𓆩 𝑴𝑬𝑹𝑶 𝑨𝑰 𓆪 | 𝐒𝐇𝐈𝐁𝐔𝐘𝐀 𝐆𝐔𝐈𝐋𝐃",
     newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+      name: '𝐒𝐇𝐈𝐁𝐔𝐘𝐀 𝐆𝐔𝐈𝐋𝐃 🕷️',
       jid: '120363225356834044@newsletter'
     },
     big: false

0fe58a37bdb275ba50c8bb71f60881c95d1ec075
Omar Tegany <omartegany8@gmail.com>
2026-06-15 10:07:47 +0300

    Update link.js

--- plugins/group/link.js
+++ plugins/group/link.js
@@ -2,26 +2,27 @@ let handler = async (m, {
     conn
 }) => {
     try {
-        m.reply(`❀° ┄──────────╮
-🌷┊ *رابـــط الـــمـــجـــمـــوعـــة:* ${(await conn.groupMetadata(m.chat)).subject}
-🌷┊
-🌷┊ https://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) + `
-🌷┊
-🌷┊ ${conn.user.name || "Bot WhatsApp"}
-╰──────────┄ °❀`)
+        m.reply(`┄────────── °❀° ┄──────────
+🌷┊ *رابـــط الـــمـــجـــمـــوعـــة:*
+🌷┊ *${(await conn.groupMetadata(m.chat)).subject}*
+🌷┊ *https://chat.whatsapp.com/` + await conn.groupInviteCode(m.chat) + `*
+🌷┊ *رابـــط الاستقبال:*
+🌷┊ ${conn.user.name || "https://chat.whatsapp.com/LUFdrmHVmlKEbkO26rOFfG
+"}
+──────────┄ °❀° ┄──────────`)
     } catch {
         const mentionedUser = conn.user.id.split(":")[0] + "@s.whatsapp.net";
         conn.sendMessage(m.chat, { 
             text: `❀° ┄──────────╮
-🌷┊ يـــجـــب تـــعـــيـــيـــن @${mentionedUser.split('@')[0]} كـــمـــشـــرف لـــلـــتـــمـــكـــن مـــن اســـتـــخـــدام هـــذا الـــأمـــر!
+🌷┊ @${mentionedUser.split('@')[0]} غير مسموح لك باستخدام هذا الامر!
 ╰──────────┄ °❀`, 
             mentions: [mentionedUser]
         })
     }
 }
-handler.usage = ["لينك"];
+handler.usage = ["رابط"];
 handler.category = "group";
-handler.command = ["لينك", "link"];
+handler.command = ["رابط", "link"];
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;

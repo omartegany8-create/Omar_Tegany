1a6389f5be74aab577a04dffb7e31af09cfae9ca
Omar Tegany <omartegany8@gmail.com>
2026-07-07 10:03:27 +0300

    Create Bot_test.js

---
+++ plugins/auto/Bot_test.js
@@ -0,0 +1,20 @@
+export default async function before(m, { conn , bot }) {
+  // الكلمات المفتاحية المطلوبة لتفعيل الرد
+  const triggers = ["بوت", "تست", "لايت"];
+
+  if (triggers.includes(m.text?.trim())) {
+    
+    // 💡 ضع رابط الفيديو المباشر الخاص بك هنا بين علامتي الاقتباس
+    const videoUrl = ""; 
+
+    await conn.sendMessage(m.chat, {
+      video: { url: videoUrl },
+      mimetype: 'video/mp4',
+      ptv: true // هذه الخاصية تجعل الفيديو يظهر على شكل فيديو ملاحظات (دائري)
+    }, { quoted: m });
+
+    return true; 
+  }
+  
+  return false;
+}
